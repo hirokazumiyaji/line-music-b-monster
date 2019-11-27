6ced18fb5ca299742eed4484bb30ed8d58bedb5e
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2019-11-27 10:52:09 +0900

    update thumbnail images

--- scrape/index.ts
+++ scrape/index.ts
@@ -1,6 +1,6 @@
 import * as argv from 'argv'
 import * as fs from 'fs-extra'
-import fetch from 'node-fetch'
+import axios from 'axios'
 import * as puppeteer from 'puppeteer'
 import * as colors from 'colors/safe'
 import * as uuidv5 from 'uuid/v5'
@@ -25,62 +25,87 @@ async function getOutputPlaylist(path: string): Promise<Output[]> {
   return JSON.parse(buffer.toString())
 }
 
-async function scrape(url: string): Promise<Output> {
+async function scrape(playlistID: string, url: string): Promise<Output> {
   console.log(colors.cyan(`start scraping ${url}`))
 
   const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.setUserAgent(ua)
-  await page.setViewport({ width: 1200, height: 800 })
-  await page.setJavaScriptEnabled(true)
-  await page.goto(url, { waitUntil: 'domcontentloaded' })
 
-  await page.waitForSelector('.playlistInfo .des .title .txt')
-  await page.waitForSelector('.playlistInfo .thumb img')
-  await page.waitFor(5000)
+  try {
+    const page = await browser.newPage()
+    await page.setUserAgent(ua)
+    await page.setViewport({ width: 1200, height: 800 })
+    await page.setJavaScriptEnabled(true)
+    await page.goto(url, { waitUntil: 'domcontentloaded' })
 
-  const title = await page.$eval('.playlistInfo .des .title .txt', (it: any) => {
-    return it.textContent
-  })
-  let thumbnails = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll('.playlistInfo .thumb img').values())
-      .map(it => it.getAttribute('src'))
-  })
-  await browser.close()
+    await page.waitForSelector('.playlistInfo .des .title .txt')
+    await page.waitForSelector('.playlistInfo .thumb img')
+    await page.waitFor(5000)
 
-  console.log(colors.cyan('finish scraping'))
+    const title = await page.$eval('.playlistInfo .des .title .txt', (it: any) => {
+      return it.textContent
+    })
+    let thumbnails = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('.playlistInfo .thumb img').values())
+        .map(it => it.getAttribute('src'))
+    })
 
-  console.log(colors.cyan('start download thumbnail'))
+    console.log(colors.cyan(`finish scraping ${url}`))
 
-  const id: string = url.replace('https://music.line.me/playlist/', '')
-
-  await fs.ensureDir(`${options.output}/assets/images/playlists/${id}`)
-
-  thumbnails = await Promise.all(thumbnails.map(async (it: string) => {
-    const response = await fetch(it)
-    const contentType = response.headers.get('content-type')
-    const buffer = response.buffer()
-    let path = `${options.output}/assets/images/playlists/${id}/${uuidv5(it, uuidv5.URL)}`
-    switch (contentType) {
-      case 'image/jpeg':
-        path = `${path}.jpg`
-        break
-      case 'image/png':
-        path = `${path}.png`
-        break
+    return {
+      title: title,
+      url: url,
+      link: `https://music.line.me/launch?cc=JP&target=playlist&item=${playlistID}`,
+      thumbnails: thumbnails
     }
-    await fs.writeFile(path, buffer)
-    return path.replace(options.output, '')
-  }))
-
-  console.log(colors.cyan('finish download thumbnail'))
-
-  return {
-    title: title,
-    url: url,
-    link: `https://music.line.me/launch?cc=JP&target=playlist&item=${id}`,
-    thumbnails: thumbnails
+  } catch (e) {
+    console.log(colors.red(`scraping error ${e.message}`))
+    throw e
+  } finally {
+    await browser.close()
   }
+}
+
+async function downloadImage(url: string, dir: string): Promise<string> {
+  let fileName = uuidv5(url, uuidv5.URL)
+
+  const response = await axios.get(url, {
+    responseType: 'stream',
+    headers: {
+      'user-agent': ua
+    }
+  })
+  const contentType = response.headers['content-type'] || ''
+  switch (contentType) {
+    case 'image/jpeg':
+      fileName = `${fileName}.jpg`
+      break
+    case 'image/png':
+      fileName = `${fileName}.png`
+      break
+  }
+
+  const path = `${dir}/${fileName}`
+  const writer = fs.createWriteStream(path)
+  response.data.pipe(writer)
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', () => resolve(path.replace(options.output, '/assets')))
+    writer.on('error', reject)
+  })
+}
+
+async function downloadImages(urls: string[], dir: string): Promise<string[]> {
+  return await Promise.all(urls.map(async (it: string) => {
+    console.log(colors.cyan(`start download thumbnail ${it}`))
+    try {
+      const path = await downloadImage(it, dir)
+      console.log(colors.cyan(`finish download thumbnail ${it}`))
+      return path
+    } catch (e) {
+      console.log(colors.red(`download thumbnail ${e.message}`))
+      throw e
+    }
+  }))
 }
 
 async function writeOutput(path: string, output: Output[]): Promise<void> {
@@ -102,7 +127,7 @@ argv.option({
 
 const options = argv.run().options
 
-const outputJsonFilePath = `${options.output}/assets/json/playlist.json`
+const outputJsonFilePath = `${options.output}/json/playlists.json`
 
 Promise.all([
   listPlaylistLinks(options.input),
@@ -112,11 +137,18 @@ Promise.all([
   let output: Output[] = result[1]
   output = await Promise.all(
     links.map(async (link: string) => {
-      const o = output.find((it: Output) => it.url === link)
+      let o = output.find((it: Output) => it.url === link)
       if (o) {
         return o
       }
-      return await scrape(link)
+
+      const playlistID: string = link.replace('https://music.line.me/playlist/', '')
+      const dir = `${options.output}/images/playlists/${playlistID}`
+      await fs.ensureDir(dir)
+
+      o = await scrape(playlistID, link)
+      o.thumbnails = await downloadImages(o.thumbnails, dir)
+      return o
     })
   )
   return output
--- web/src/App.vue
+++ web/src/App.vue
@@ -8,17 +8,17 @@
         contain
         src="/assets/images/line-music.jpg"
         transition="scale-transition"
-        height="64"
+        height="56"
         width="100"
       />
       <v-icon style="color: #000000; font-weight: bold;">mdi-close</v-icon>
       <v-img
         alt="b-monster Logo"
-        class="shrink mt-1"
+        class="shrink ml-2"
         contain
-        src="/assets/images/b-monster.jpg"
-        height="50"
-        width="100"
+        src="/assets/images/b-monster.png"
+        height="56"
+        width="112"
       />
     </div>
   </v-app-bar>
